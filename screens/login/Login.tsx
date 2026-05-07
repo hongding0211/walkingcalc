fb41186909791a54714a37268efae97a7ee48d3d
HongDing <keith.dh@hotmail.com>
2026-05-07 17:35:52 +0800

    Use internal SSO webview login

--- screens/login/Login.tsx
+++ screens/login/Login.tsx
@@ -1,75 +1,24 @@
-import * as Linking from 'expo-linking'
-import * as WebBrowser from 'expo-web-browser'
+import { useNavigation } from '@react-navigation/native'
 import React, { useCallback, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Image, View } from 'react-native'
-import { useDispatch } from 'react-redux'
 
 import Button from '../../components/General/Button'
 import ThemedText from '../../components/General/Themed/Text'
 import ThemedView from '../../components/General/Themed/View'
-import useToast from '../../components/Toast/useToast'
-import { SSO_LOGIN_URL } from '../../constants/Config'
-import { setLoading } from '../../feature/general/generalSlice'
-import { setToken } from '../../feature/user/userSlice'
+import { LoginProps } from '../../navigation/types'
 import styles from './style'
-
-WebBrowser.maybeCompleteAuthSession()
-
-const tokenFromRedirectUrl = (url: string) => {
-  const hash = url.split('#')[1]
-  if (!hash) {
-    return undefined
-  }
-
-  const params = new URLSearchParams(hash)
-  return (
-    params.get('accessToken') ||
-    params.get('token') ||
-    decodeURIComponent(hash.split('&')[0])
-  )
-}
 
 const Login: React.FC = () => {
   const { t } = useTranslation('login')
-  const dispatch = useDispatch()
-  const toast = useToast()
+  const navigation = useNavigation<LoginProps['navigation']>()
   const [isSigningIn, setIsSigningIn] = useState(false)
 
-  const login = useCallback(async () => {
-    const redirectUrl = Linking.createURL('auth/callback')
-    const authUrl = new URL(SSO_LOGIN_URL)
-    authUrl.searchParams.set('redirect', redirectUrl)
-
+  const login = useCallback(() => {
     setIsSigningIn(true)
-    dispatch(setLoading({ status: true }))
-    try {
-      const result = await WebBrowser.openAuthSessionAsync(
-        authUrl.toString(),
-        redirectUrl
-      )
-      if (result.type !== 'success') {
-        return
-      }
-
-      const token = tokenFromRedirectUrl(result.url)
-      if (!token) {
-        toast(t('loginFail') + '')
-        return
-      }
-
-      dispatch(
-        setToken({
-          token,
-        })
-      )
-    } catch {
-      toast(t('loginFail') + '')
-    } finally {
-      setIsSigningIn(false)
-      dispatch(setLoading({ status: false }))
-    }
-  }, [dispatch, t, toast])
+    navigation.navigate('SSO')
+    setIsSigningIn(false)
+  }, [navigation])
 
   return (
     <ThemedView style={{ flex: 1 }}>
--- screens/modal/login/SSO.tsx
+++ screens/modal/login/SSO.tsx
@@ -1,34 +1,101 @@
 import { useNavigation } from '@react-navigation/native'
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useMemo, useRef, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import WebView from 'react-native-webview'
+import { useDispatch } from 'react-redux'
 
-import { SSO_URL } from '../../../constants/Config'
+import useToast from '../../../components/Toast/useToast'
+import { HONG97_WEB_BASE_URL, SSO_LOGIN_URL } from '../../../constants/Config'
+import { setLoading } from '../../../feature/general/generalSlice'
+import { setToken } from '../../../feature/user/userSlice'
 import { LoginProps } from '../../../navigation/types'
+
+const queryValue = (source: string, key: string) => {
+  const params = source.split('&')
+  const match = params.find(item => item.split('=')[0] === key)
+  const value = match?.split('=').slice(1).join('=')
+  return value ? decodeURIComponent(value) : undefined
+}
+
+const tokenFromRedirectUrl = (url: string) => {
+  const hash = url.split('#')[1]
+  if (hash) {
+    return (
+      queryValue(hash, 'accessToken') ||
+      queryValue(hash, 'token') ||
+      decodeURIComponent(hash.split('&')[0])
+    )
+  }
+
+  const query = url.split('?')[1]
+  if (!query) {
+    return undefined
+  }
+  return (
+    queryValue(query.split('#')[0], 'accessToken') || queryValue(query, 'token')
+  )
+}
 
 const SSO: React.FC = () => {
   const [showWebview, setShowWebView] = useState(true)
+  const hasHandledRedirect = useRef(false)
   const navigation = useNavigation<LoginProps['navigation']>()
+  const dispatch = useDispatch()
+  const toast = useToast()
+  const { t } = useTranslation('login')
 
-  const handleNavStateChange = useCallback((state: any) => {
-    const { url } = state
-    const decodedUrl = decodeURIComponent(url)
-    const match = decodedUrl.match(/\?ticket=(.+)/)
-    if (!match || match.length < 2) {
-      return
-    }
-    navigation.navigate('Login', {
-      ticket: match[1],
-      type: 'sso',
-    })
-    setShowWebView(false)
-  }, [])
+  const redirectUrl = useMemo(() => `${HONG97_WEB_BASE_URL}/auth/callback`, [])
+  const authUrl = useMemo(
+    () => `${SSO_LOGIN_URL}?redirect=${encodeURIComponent(redirectUrl)}`,
+    [redirectUrl]
+  )
+
+  const handleRedirect = useCallback(
+    (url: string) => {
+      if (hasHandledRedirect.current) {
+        return true
+      }
+
+      if (!url.startsWith(redirectUrl)) {
+        return false
+      }
+
+      const token = tokenFromRedirectUrl(url)
+      if (!token) {
+        hasHandledRedirect.current = true
+        toast(t('loginFail') + '')
+        return true
+      }
+
+      hasHandledRedirect.current = true
+      setShowWebView(false)
+      dispatch(setLoading({ status: true }))
+      dispatch(
+        setToken({
+          token,
+        })
+      )
+      dispatch(setLoading({ status: false }))
+      navigation.goBack()
+      return true
+    },
+    [dispatch, navigation, redirectUrl, t, toast]
+  )
+
+  const handleNavStateChange = useCallback(
+    (state: any) => {
+      handleRedirect(state.url)
+    },
+    [handleRedirect]
+  )
 
   return (
     <>
       {showWebview && (
         <WebView
-          source={{ uri: SSO_URL }}
+          source={{ uri: authUrl }}
           onNavigationStateChange={handleNavStateChange}
+          onShouldStartLoadWithRequest={request => !handleRedirect(request.url)}
           cacheEnabled={false}
           injectedJavaScript="(function(){localStorage.clear()})()"
         />
